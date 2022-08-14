acabfb89db1d21db663f8791620b54de602f84ab
farukprogrammer <faruk.abdul@bukalapak.com>
2022-08-15 06:56:17 +0700

    add new icon + change app name to EBookApp

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:goat_flutter_challange/main.dart';
+import 'package:goat_flutter_challange/src/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
